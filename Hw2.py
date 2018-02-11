d3aa3b73ccffe30914309ca83aa65466aeadb426
Jonathan Semeyn <semeynjo@msu.edu>
2018-02-11 14:33:06 -0500

    finished notebook

--- Hw2.py
+++ Hw2.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 v_0 = 100 #m/s
-r = 10 #cm
+r = 0.10 #m
 d = 11.34 #g/cm^3
 angle = np.deg2rad(40) #converted degrees to radians for np.sin
 g = 9.81 #m/s^2
@@ -26,6 +26,8 @@ def midpoint(posx,posy,velx,vely,accelx,accely,dt):
 
 d_true =  v_0**2*np.sin(2*angle)/g
 
+# Finding distance using Euler
+
 vx0 = v_0*np.cos(angle)
 vy0 = v_0*np.sin(angle)
 x0 = 0
@@ -38,7 +40,7 @@ vx1 = [vx0]
 vy1 = [vy0]
 
 t=0
-dt=.4
+dt=.098
 
 while y0>=0:
     
@@ -54,14 +56,16 @@ while y0>=0:
     vy1.append(vy0)
     t1.append(t)
     
-plt.plot(x1,y1)
+#plt.plot(x1,y1)
 y1.index(max(y1))
     
 E = np.abs(d_true - x1[-1])/d_true*100
 
-print('The error is: {0:.2f}%, The calculated distance is: {1:.2f}, The true \
-distance is: {2:.2f}, The number of steps is: {3:}'.format(E,x1[-1],d_true,\
-len(x1)))
+#print('The error is: {0:.2f}%, The calculated distance is: {1:.2f}, The true \
+#distance is: {2:.2f}, The number of steps is: {3:}'.format(E,x1[-1],d_true,\
+#len(x1)))
+
+#Finding distance using midpoint
 
 vx0 = v_0*np.cos(angle)
 vy0 = v_0*np.sin(angle)
@@ -91,12 +95,62 @@ while y0>=0:
     vy2.append(vy0)
     t2.append(t)
 
-plt.plot(x2,y2)
-y2.index(max(y2))
-print(max(x2))
+#plt.plot(x2,y2)
+#y2.index(max(y2))
+#print(max(x2))
     
 E = np.abs(d_true - x2[-1])/d_true*100
 
-print('The error is: {0:.2f}%, The calculated distance is: {1:.2f}, The true \
-distance is: {2:.2f}, The number of steps is: {3:}'.format(E,x2[-1],d_true,\
-len(x2)))
+#print('The error is: {0:.2f}%, The calculated distance is: {1:.2f}, The true \
+#distance is: {2:.2f}, The number of steps is: {3:}'.format(E,x2[-1],d_true,\
+#len(x2)))
+
+#Adding air resistance to Euler
+
+C = .47
+p = 1.2 #density of air, kg/m^3
+A = np.pi*r**2
+m = 475 #kg
+
+vx0 = v_0*np.cos(angle)
+vy0 = v_0*np.sin(angle)
+ax0 = (-1/2*C*p*A*vx0**2*(vx0/np.abs(vx0)))/m
+ay0 = -g+(-1/2*C*p*A*vy0**2*(vy0/np.abs(vy0)))/m
+x0 = 0
+y0 = 0
+
+x3=[0]
+y3=[0]
+t3=[0]
+vx3 = [vx0]
+vy3 = [vy0]
+ax3=[ax0]
+ay3=[ay0]
+
+t=0
+dt=.07
+
+while y0>=0:
+    
+    vx,vy,x,y = fEuler(x0,y0,vx0,vy0,ax0,ay0,dt)
+    
+    ax = (-1/2*C*p*A*vx**2*(vx/np.abs(vx)))/m
+    ay = -g+(-1/2*C*p*A*vy**2*(vy/np.abs(vy)))/m
+    
+    ax0,ay0,vx0,vy0,x0,y0 = ax,ay,vx,vy,x,y
+    
+    t += dt
+    
+    x3.append(x0)
+    y3.append(y0)
+    vx3.append(vx0)
+    vy3.append(vy0)
+    ax3.append(ax0)
+    ay3.append(ay0)
+    t3.append(t)
+
+plt.plot(x3,y3)
+y3.index(max(y3))
+
+print('The calculated distance is: {0:.2f}, The number of steps is: {1:}'\
+      .format(x3[-1],len(x3)))
